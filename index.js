6f8b49ddfc0a4818750dae32c03f096cc81df280
gsmith77 <grantsmith1256@gmail.com>
2019-10-01 23:36:59 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function findMinAndRemove(array){
     let min = array[0]
     let minIndex = 0
-    for(let i=0; i<array.length - 1; i++){
+    for(let i=0; i<array.length; i++){
         if(array[i] < min){
             min = array[i]
             minIndex = i
