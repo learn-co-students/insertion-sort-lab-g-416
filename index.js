15596698a73074b1991bb193083129395396eff8
Jeremiah Rodden <jrodden1@gmail.com>
2020-02-04 11:33:03 -0700

    findMinAndRemove fn working

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+// Find the minimum number in an array and destructively remove that element from the array and return it.
+// array = [2, 3, 4, 5, 6, 7]
 function findMinAndRemove(array){
-
+   let min = array[0]
+   let minIndex = 0
+   array.forEach((elem, i) => {
+      if (elem < min) {
+         min = elem
+         minIndex = i
+      }
+   })
+   array.splice(minIndex, 1)
+   return min
 }
+let array = [3, 2, 4, 5, 6, 7]
+console.log("findMinAndRemove: ", findMinAndRemove(array))
+console.log("array afterward: ", array)
 
 function insertionSort(array){
 
