944100fc852ab0219713685ab54c775f8d4585b6
Dominique Skaf <doskaf@yahoo.com>
2020-06-19 11:37:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+    let min = array[0];
+    let minIndex = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+            minIndex = i;
+        }
+    }
+    array.splice(minIndex, 1);
+    return min;
 }
 
 function insertionSort(array){
-
+    let currentMin;
+    let sortedArray = [];
+    while (array.length != 0) {
+        currentMin = findMinAndRemove(array);
+        sortedArray.push(currentMin);
+    }
+    return sortedArray;
 }
