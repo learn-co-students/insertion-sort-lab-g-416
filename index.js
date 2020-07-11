0a6dd560142f4d55a6d363abbaa06d08b1f9e9e0
dvfleet413 <dvfleet413@gmail.com>
2020-07-11 11:25:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+    let min = array[0]
+    let minIndex;
+    for(let i = 0; i < array.length; i++){
+        if(array[i] < min){
+            min = array[i]
+            minIndex = i
+        }
+    }
+    array.splice(minIndex, 1);
+    return min;
 }
 
 function insertionSort(array){
-
+    const newArray = []
+    while(array.length > 0){
+        newArray.push(findMinAndRemove(array))
+    }
+    return newArray;
 }
