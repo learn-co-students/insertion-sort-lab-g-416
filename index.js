0dc5736ee9aa7e016c3db8bf7f8bcf5f6652a384
dean <deanlloyd64@gmail.com>
2020-08-06 17:40:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+    let minVal = array[0]
+    let minIndex
+    for(let i = 0; i < array.length; i++){
+        if(minVal > array[i]){
+            minVal = array[i]
+            minIndex = i
+        }
+    }
+    array.splice(minIndex, 1)
+    return minVal
 }
 
 function insertionSort(array){
-
+    let sorted = []
+    while(array.length !== 0){
+        sorted.push(findMinAndRemove(array))
+    }
+    return sorted
 }
