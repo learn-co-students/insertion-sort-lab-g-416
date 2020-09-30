399695e79cad3b915bf1ad1c5b44a6aa9394d838
rschlosser11 <rosalinschlosser@gmail.com>
2020-09-30 11:25:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+    let min = array[0];
+    let minI;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+            minI = i;
+        }
+    }
+    array.splice(minI, 1);
+    return min
 }
 
 function insertionSort(array){
-
+    let sorted = [];
+    while (array.length !== 0) {
+        console.log(array);
+        let min = findMinAndRemove(array)
+        sorted.push(min);
+    }
+    return sorted;
 }
