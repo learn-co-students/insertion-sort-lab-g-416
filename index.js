a2bd41d224425cebe5db6085b6139b070908616d
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-10-01 14:48:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function findMinAndRemove(array){
+function findMinAndRemove(array) {
+  let minValue = array[0];
+  let minIndex = 0;
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < minValue) {
+      minValue = array[i];
+      minIndex = i;
+    }
+  }
 
+  array.splice(minIndex, 1);
+  return minValue;
 }
 
-function insertionSort(array){
+function insertionSort(array) {
+  let sortedArray = [];
+  while (array.length !== 0) {
+    sortedArray.push(findMinAndRemove(array));
+  }
 
+  return sortedArray;
 }
