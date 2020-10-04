baa3e1f4767a3a79dad34d82c8bd75918423c13b
nathan-stamand <nathansaint810@gmail.com>
2020-10-04 03:35:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function findMinAndRemove(array){
-
+    let min = array[0], minIndex;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+            minIndex = i;
+        }
+    }
+    array.splice(minIndex, 1)
+    return min;
 }
 
 function insertionSort(array){
-
+    let sortyArr = [],
+        newMin;
+    while (array.length > 0) {
+        newMin = findMinAndRemove(array);
+        sortyArr.push(newMin);
+    }
+    return sortyArr;
 }
