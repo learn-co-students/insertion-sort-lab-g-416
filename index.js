47b6c2e37e02e3055b89cd23b533aaa829384807
tyler jones <rteyl1690@gmail.com>
2021-01-05 20:31:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function findMinAndRemove(array){
-
+  let min = array[0], ind = 0;
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] < min){
+      ind = i
+      min = array[i]
+    }
+  }
+  array.splice(ind, 1)
+  return min
 }
 
 function insertionSort(array){
-
+  let sorted = [];
+  while (array.length !== 0) {
+    sorted.push(findMinAndRemove(array));
+  }
+  return sorted;
 }
