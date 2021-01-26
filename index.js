9fc7547565571d1a64aa70bab15d7e25da29f9f2
blumenthaler <blumenthaleralex@gmail.com>
2021-01-26 11:54:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function findMinAndRemove(array){
-
+    // find the minimum in an array and remove that element
+    // [2, 3, 8, 24, 1]
+    let min = array[0]
+    let minIndex = 0
+    for (let i=0; i<array.length; i++) {
+        if (array[i] < min) {
+            min = array[i]
+            minIndex = i
+        }
+    }
+    array.splice(minIndex, 1)
+    return min
 }
 
 function insertionSort(array){
-
+    let newArray = []
+    while (array.length != 0) {
+        let newMin = findMinAndRemove(array)
+        newArray.push(newMin)
+    }
+    return newArray
 }
