663b818cbd4c0fa02d78dbe58fdc5da57c1cadd6
ViolaCello <ViolaCelloNYC@gmail.com>
2021-01-29 13:52:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 function findMinAndRemove(array){
-    let newArray = array.sort()
-    return newArray.splice(0,1)
+    let min=array[0]
+    let index = 0
+    for (let i=1; i<array.length; i++) {
+        if (array[i]<min) {
+            min=array[i]
+            index=i
+        }
+        array.splice[index,1]
+        return min
+    }
+
+ 
 }
 
 function insertionSort(array){
+let newArray;
 
 }
