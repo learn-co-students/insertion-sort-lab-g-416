03804b2d6d72ae15199b7585214d79a0e2835327
Olesya Miller <olesyam.miller@gmail.com>
2021-02-25 12:28:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function findMinAndRemove(array){
+    let min = array[0]
+    for(let i = 0; i < array.length; i ++) {
+        if(array[i] < min) {
+            min = array[i]
+        }
+        // array.splice(array.indexOf(min), array.indexOf(min) +1)
+    }
+    array.splice(array.indexOf(min), 1) // we need this line for our insertionSort function
+    return min 
 
 }
 
 function insertionSort(array){
+    let newArray = []
+    while(array.length > 0) {
+        newArray.push(findMinAndRemove(array))
+    }
+    return newArray
 
 }
